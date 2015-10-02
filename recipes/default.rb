d3d9dd528119e18f28bc398d685891af59a6812b
Aaron Borden <adborden@a14n.net>
2015-10-01 23:07:00 -0700

    Use nvm cookbook

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,10 +2,19 @@ package 'python-virtualenv'
 package 'libmysqlclient-dev'
 package 'libpq-dev'
 package 'python-dev'
-package 'nodejs'
-package 'npm'
 
 group 'opencal'
+
+include_recipe 'nvm'
+nvm_install 'v0.10.39'  do
+    user 'backend'
+    group 'opencal'
+    user_install true
+    user_home '/data/backend'
+    from_source false
+    alias_as_default true
+    action :create
+end
 
 include_recipe 'disclosure-cookbook::_nginx'
 include_recipe 'disclosure-cookbook::_cronjobs'
