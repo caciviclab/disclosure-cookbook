73720282ca36d37a9066b6063600836e7286e136
Aaron Borden <adborden@a14n.net>
2015-10-01 20:02:49 -0700

    opencal.opendisclosure.io -> disclosure.caciviclab.org

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Disclosure Backend Cookbook
 This cookbook configures a backend server for the disclosure-backend python
 app. The prerequisites to use this are:
 
-1. SSH access to the server (in our case, opencal.opendisclosure.io)
+1. SSH access to the server (in our case, disclosure.caciviclab.org)
 2. Passwordless sudo to that server, e.g. `%opencal ALL=(ALL:ALL) NOPASSWD:ALL`
 
 To run this cookbook on the server, simply run the script:
--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 set -euo pipefail
 
 echo 'Installing chef...'
-ssh opencal.opendisclosure.io 'if [ ! $(which chef-solo) ]; then curl -L https://www.chef.io/chef/install.sh | sudo bash; fi'
+ssh disclosure.caciviclab.org 'if [ ! $(which chef-solo) ]; then curl -L https://www.chef.io/chef/install.sh | sudo bash; fi'
 
 echo 'Installing /etc/chef/solo.rb'
-ssh opencal.opendisclosure.io /bin/bash -c "
+ssh disclosure.caciviclab.org /bin/bash -c "
 sudo mkdir -p /etc/chef /data/cookbook;
 sudo chown backend:opencal /data/cookbook -R;
 echo 'cookbook_path \"/data/cookbook\"' | sudo tee /etc/chef/solo.rb
@@ -13,5 +13,5 @@ echo 'cookbook_path \"/data/cookbook\"' | sudo tee /etc/chef/solo.rb
 
 echo 'Deploying...'
 rsync --rsync-path 'sudo -u backend rsync' \
-  --delete --archive . opencal.opendisclosure.io:/data/cookbook/disclosure-cookbook
-ssh opencal.opendisclosure.io 'sudo chef-solo -o "recipe[disclosure-cookbook]" '
+  --delete --archive . disclosure.caciviclab.org:/data/cookbook/disclosure-cookbook
+ssh disclosure.caciviclab.org 'sudo chef-solo -o "recipe[disclosure-cookbook]" '
